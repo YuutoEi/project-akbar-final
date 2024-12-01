12713553844393fae7364bf0892bb1e1c0d57e6c
YuutoEi <anadrydhandy@gmail.com>
2024-12-01 17:15:41 +0700

    test

---
+++ landing-page/JS/script.js
@@ -0,0 +1,9 @@
+function test() {
+    document.getElementById("header").style.backgroundColor = "rgb(59, 59, 59)";
+    document.getElementById("body").style.backgroundColor = "rgb(59, 59, 59)";
+    document.getElementById("footer").style.backgroundColor = "rgb(59, 59, 59)";
+    document.getElementById("footer").style.boxShadow = "0px 3px 5px black inset";
+
+}
+
+    
